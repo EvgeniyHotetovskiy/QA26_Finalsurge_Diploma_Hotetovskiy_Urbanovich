0677121aee5f018c1a4f995e79fd1d4bb3a7cb04
EvgeniyHotetovskiy <squaer.r@mail.ru>
2024-07-15 21:11:46 +0300

    Refactor for selenide

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,6 +4,9 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class LoginPage extends BasePage {
     private static final  By inputEmailField = By.cssSelector("#login_name");
     private static final By inputPasswordField = By.cssSelector("#login_password");
@@ -14,23 +17,18 @@ public class LoginPage extends BasePage {
     }
 
     public void setEmailValue (String email) {
-        driver.findElement(inputEmailField).sendKeys(email);
+        $(inputEmailField).setValue(email);
     }
     public void setPasswordValue (String password) {
-        driver.findElement(inputPasswordField).sendKeys(password);
+        $(inputPasswordField).setValue(password);
     }
     public void clickLoginButton () {
-        driver.findElement(loginButton).click();
+        $(loginButton).shouldBe(visible).click();
     }
     @Step ("Авторизироваться с логином {email} и паролем {password}")
     public void login(String email, String password) {
         setEmailValue(email);
         setPasswordValue(password);
         clickLoginButton();
-    }
-
-    @Step("Открыть сайт FinalSurge")
-    public void open (String url) {
-        driver.get(url);
     }
 }
